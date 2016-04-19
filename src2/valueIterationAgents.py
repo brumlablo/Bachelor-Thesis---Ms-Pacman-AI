e41c32feb96e017d42574313b35834ebd257e2f0
babu <barbora.blozonova@gmail.com>
2016-04-20 01:08:15 +0200

    value iteration basic agent finished, documentation updated

--- src2/valueIterationAgents.py
+++ src2/valueIterationAgents.py
@@ -43,9 +43,26 @@ class ValueIterationAgent(ValueEstimationAgent):
         self.iterations = iterations
         self.values = util.Counter() # A Counter is a dict with default 0
 
-        # Write value iteration code here
-        for i in iterations:
-            pass
+        for k in range(0,iterations): # actual values = Value_k, counting new values Value_{k+1} = Value_k
+            tmpValues = self.values.copy() # current values V_k
+            for state in self.mdp.getStates():
+                # legal actions for state
+                actions = self.mdp.getPossibleActions(state)
+                if mdp.isTerminal(state) or len(actions) == 0:
+                    tmpValues[state] = 0
+                    continue
+                # maximalize = gain best possible action based on chance q-values
+                value = float("-inf") # V_{k+1}
+                for action in actions:
+                    qvals = 0.0 # suma of q-values
+                    qvals = self.getQValue(state, action)
+                    # same as ...
+                    # for nextState, probability in self.mdp.getTransitionStatesAndProbs(state, action):
+                    #     tmp += probability * (self.mdp.getReward(state, action, nextState) + self.discount * self.getValue(nextState))
+                    if qvals > value: # best value possible
+                        value = qvals
+                tmpValues[state] = value
+            self.values = tmpValues # update values for each state V_{k+1}
 
     def getValue(self, state):
         """
@@ -54,32 +71,50 @@ class ValueIterationAgent(ValueEstimationAgent):
         return self.values[state]
 
 
-    def computeQValueFromValues(self, state, action):
+    def computeQValueFromValues(self, state, action): #chance node
         """
           Compute the Q-value of action in state from the
           value function stored in self.values.
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
 
-    def computeActionFromValues(self, state):
+          probability = transitionFunction(state,action,nextState)
+          Q_k(state) = suma(probability * ( Reward(state,action,nextState) + discountFactor * Value_k(nextState)
+        """
+        qval = 0.0
+        for nextState,probability in self.mdp.getTransitionStatesAndProbs(state, action):
+            qval += probability * ( self.mdp.getReward(state,action,nextState) + self.discount * self.getValue(nextState))
+        return qval
+
+    def computeActionFromValues(self, state): # max node
         """
           The policy is the best action in the given state
           according to the values currently stored in self.values.
 
-          You may break ties any way you see fit.  Note that if
-          there are no legal actions, which is the case at the
-          terminal state, you should return None.
+          Choosing optimal policy (similar to max node in Expectimax)
+          Value*(state) = max action of Q-Value*(state) (state,action,nextState)
+          = = PI*
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # tuple (action,value)
+        expectedValue = ["", float("-inf")]
 
+        actions = self.mdp.getPossibleActions(state)
+        # terminal node
+        if len(actions) == 0:
+            return None
+
+        for action in actions:
+            tmp = self.getQValue(state,action) # expected value
+            if tmp > expectedValue[1]:
+                expectedValue = (action,tmp)
+        return expectedValue[0]
+
+    # max node
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
+
+    # chance node
+    def getQValue(self, state, action):
+        return self.computeQValueFromValues(state, action)
 
     def getAction(self, state):
         "Returns the policy at the state (no exploration)."
         return self.computeActionFromValues(state)
-
-    def getQValue(self, state, action):
-        return self.computeQValueFromValues(state, action)
