97bc0dd66b9485509499807655346299c05f6fc3
Ondrej Hajek <ondrejhajek.oh@gmail.com>
2016-05-14 17:54:24 +0200

    added 3rd experiments expectimax tests

--- src/expectonasrat.sh
+++ src/expectonasrat.sh
@@ -1,13 +1,41 @@
 #!bin/bash
 
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta1.txt
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta2.txt
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta3.txt
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta4.txt
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta5.txt
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta6.txt
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta7.txt
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta8.txt
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta9.txt
-python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta10.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta87.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta97.txt
+
+
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta18.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta28.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta38.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta48.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta58.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta68.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta78.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta88.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta98.txt
+
+
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta19.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta29.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta39.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta49.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta59.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta69.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta79.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta89.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta99.txt
+
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta20.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta30.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta40.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta50.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta60.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta70.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta80.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta90.txt
+
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta100.txt
+
+
+
 
