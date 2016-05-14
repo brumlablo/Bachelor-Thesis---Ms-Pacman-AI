d067a4cc9371db1da1ffa691bee0d5bb5c56760f
Ondrej Hajek <ondrejhajek.oh@gmail.com>
2016-05-14 04:40:14 +0200

    expreimenty 1,2 a cast 3

---
+++ src/expectonasrat.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta1.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta2.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta3.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta4.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta5.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta6.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta7.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta8.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta9.txt
+python2 pacman.py -p ExpectimaxAgent -l trickyClassic -n 1 --frameTime 0 | tee exper/3/tricky_expecta10.txt
+
